643154e1bc136a497587b9d2b6caa325dce7b52a
Xavier Ochoa <xavier.ochoa@gmail.com>
2025-09-04 15:01:58 -0400

    Change python3 to python in entrypoint script

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$DOWNLOAD_MODEL" = "true" ]; then
-    python3 download_model.py --output api/src/models/v1_0
+    python download_model.py --output api/src/models/v1_0
 fi
 
 exec uv run --extra $DEVICE --no-sync python3 -m uvicorn api.src.main:app --host 0.0.0.0 --port 8880 --log-level debug
